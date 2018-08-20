add7d38e6a2c28bc648d58b4a8c1641a208c1a11
rmzord <udrea.florin92@gmail.com>
2018-08-20 13:16:01 +0300

    TEST!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        printeaza("Acasa");
+
+    }
+
+    private static void printeaza(String prints){
+         System.out.println(prints);
     }
 }
